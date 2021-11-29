c26a80a4cddfcd063c619fc20eb54d80f51eec62
DanielMerser1 <91954962+M2101479@users.noreply.github.com>
2021-11-29 11:48:17 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+import random
+import string
+from random import randint
+
+Team={
+  'See':'Rock',
+  'Dock': 'Red',
+  'Not':'Fort',
+  'Hat':'Pat',
+}
+
+
+print(Team.get('See'))
+
+
+thing=random.choice(list(Team.values()))
+print(thing)
+
+def random_string(length):
+  result = ''.join((random.choice(string.ascii_lowercase)for x in range (length)))
+  return result
+
+
+
+def generate_dict():
+  for i in range (10):
+     print("'" + random_string(random.randint(1,10)) + "': '" + random_string(random.randint(1,10)) + "'")
+
+generate_dict()
+
+random_string(generate_dict)
+
+
+ 
